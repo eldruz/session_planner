c1effb460b1ce3cb2cc8e929aa567f0cf7821a20
eldruz <eldruz@gmail.com>
2013-07-25 16:08:02 +0200

    Test de vérification du fonctionnement de Meteor

---
+++ client/session_planner.js
@@ -0,0 +1,9 @@
+var sessions = [
+  {name: 'SacMania', members: 3, last_activity: '1 minute ago'},
+  {name: 'B.O.U.B.S.', members: 2, last_activity: '5 minutes ago'},
+  {name: 'Ranking 3HitCombo', members: 0, last_activity: '3 days ago'}
+];
+
+Template.sessions.helpers({
+  sessions: sessions
+});

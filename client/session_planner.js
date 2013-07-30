1e2e40d30f1212dc10ccad8ec8bbcd8536bf1da3
eldruz <eldruz@gmail.com>
2013-07-30 16:47:45 +0200

    encore des petits tests en suivant le tutorial

--- client/session_planner.js
+++ client/session_planner.js
@@ -1,9 +1,17 @@
-var sessions = [
-  {name: 'SacMania', members: 3, last_activity: '1 minute ago'},
-  {name: 'B.O.U.B.S.', members: 2, last_activity: '5 minutes ago'},
-  {name: 'Ranking 3HitCombo', members: 0, last_activity: '3 days ago'}
-];
+Meteor.Router.add({
+  '/': 'home',
+  '/sessions/:id': function(id) {
+    Session.set('currentSessionId', id);
+    return 'session';
+  }
+});
 
 Template.sessions.helpers({
-  sessions: sessions
+  sessions: function() { return Dosage.find(); }
 });
+
+Template.session.helpers({
+  session: function() {return Dosage.findOne(Session.get('currentSessionId')); }
+});
+
+

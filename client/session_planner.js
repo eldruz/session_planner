3850ab4b5660ebc1a63bd3ac3467887cb9ad4e8f
eldruz <eldruz@gmail.com>
2013-07-30 17:40:50 +0200

    travail sur la première version fonctionnelle

--- client/session_planner.js
+++ client/session_planner.js
@@ -1,17 +1,7 @@
 Meteor.Router.add({
   '/': 'home',
-  '/sessions/:id': function(id) {
-    Session.set('currentSessionId', id);
-    return 'session';
-  }
 });
 
-Template.sessions.helpers({
+Template.liste_sessions.helpers({
   sessions: function() { return Dosage.find(); }
-});
-
-Template.session.helpers({
-  session: function() {return Dosage.findOne(Session.get('currentSessionId')); }
-});
-
-
+})

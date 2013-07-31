41cd9bb538705c3953a3c3529599c634ce3b04d2
eldruz <eldruz@gmail.com>
2013-07-31 17:42:46 +0200

    Liste de session + formulaire pour en entrer de nouvelles

--- client/session_planner.js
+++ client/session_planner.js
@@ -1,7 +1,36 @@
 Meteor.Router.add({
-  '/': 'home',
+  '/': 'liste_sessions',
+  '/creation': 'creation'
 });
 
 Template.liste_sessions.helpers({
   sessions: function() { return Dosage.find(); }
 })
+
+function creation_submit() {
+    form={};
+
+    $.each($('#creation-form').serializeArray(), function() {
+        form[this.name] = this.value;
+    });
+
+    //do validation on form={firstname:'first name', lastname: 'last name', email: 'email@email.com'}
+
+    Dosage.insert(form, function(err) {
+        if(!err) {
+            alert("Submitted!");
+            $('#creation-form')[0].reset();
+        }
+        else
+        {
+            alert("Something is wrong");
+            console.log(err);
+        }
+    });
+
+}
+
+Template.creation.events({'submit' : function() {
+    creation_submit();
+    event.preventDefault();
+}});

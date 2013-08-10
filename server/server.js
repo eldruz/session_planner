e81bf4fa6320beb9de3dd96f5ede04e18374ea5e
eldruz <eldruz@gmail.com>
2013-08-10 19:31:30 +0200

    Ajout du code côté serveur Que j'avais completement oublié d'inclure jusqu'ici, lalala...

---
+++ server/server.js
@@ -0,0 +1,16 @@
+// Publish fonctions
+
+// On ne publie que les session publiques ou les privées crées par soi-même
+Meteor.publish("sessions", function () {
+  if (this.userId)
+    return Dosage.find({
+      $or: [{"open": true}, {"owner": this.userId}]
+    }, {sort: [["date", "asc"], ["nom", "asc"]]});
+  else
+    return Dosage.find({"open": true});
+});
+
+// On publie le nom des utilisateurs pour pouvoir les afficher en tant que créateur et participants aux sessions
+Meteor.publish("users", function() {
+  return Meteor.users.find({}, {fields: {emails: 1, username: 1}});
+});

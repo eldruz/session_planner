b71477b9dd722561e8509762960846614df92799
eldruz <eldruz@gmail.com>
2013-08-14 17:06:43 +0200

    code cleanup

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@
 // On ne publie que les session publiques ou les privées crées par soi-même
 Meteor.publish("sessions", function () {
   if (this.userId)
-    return Dosage.find({
-      $or: [{"open": true}, {"owner": this.userId}, {"invited": this.userId}]
-    }, {sort: [["date", "asc"], ["nom", "asc"]]});
+    return Dosage.find(
+      {$or: [{"open": true}, {"owner": this.userId}, {"invited": this.userId}]},
+      {sort: [["date", "asc"], ["nom", "asc"]]}
+      );
   else
     return Dosage.find({"open": true});
 });

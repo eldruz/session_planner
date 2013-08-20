cae2ff9dfefe5e8bdd72660da1451114240f6938
eldruz <eldruz@gmail.com>
2013-08-20 22:18:51 +0200

    Added publications for past, current and all sessions

--- server/server.js
+++ server/server.js
@@ -13,6 +13,29 @@ Meteor.publish("sessions", function () {
     return Dosage.find({"open": true});
 });
 
+// Publish all authorized sessions, regardless of dates
+Meteor.publish("sessions_all", function () {
+  if (this.userId)
+    return Dosage.find(
+        {$or: [{"open": true}, {"owner": this.userId}, {"invited": this.userId}]}
+    );
+  else
+    return Dosage.find({"open": true});
+});
+
+// Only publish past authorized sessions
+Meteor.publish("sessions_old", function () {
+  if (this.userId)
+    return Dosage.find(
+      {$and:[
+        {$or: [{"open": true}, {"owner": this.userId}, {"invited": this.userId}]},
+        {'date': {$lt: new Date()}}
+      ]}
+    );
+  else
+    return Dosage.find({"open": true});
+});
+
 // On publie le nom des utilisateurs pour pouvoir les afficher en tant que créateur et participants aux sessions
 Meteor.publish("users", function() {
   return Meteor.users.find({}, {fields: {emails: 1, username: 1}});
